e095aaec897e39cf5d3fab9be2d31c182b677f01
maysaa1992 <batainehmaysaa@gmail.com>
2023-07-19 14:56:01 +0000

    lab29

--- accounts/admin.py
+++ accounts/admin.py
@@ -9,6 +9,23 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['email', 'username',]
+    list_display = ['email', 'username','first_name']
+    # fieldsets= UserAdmin.fieldsets +(
+    #     ('Contact Information',{
+    #         'fields': ('phone_number',)
+    #     }),
+    # )
+    fieldsets = (
+        ('Auth Info', {
+            'fields' : ('username','password','email')
+
+        }),
+        ('Personal Info',{
+         'fields' : ('first_name','last_name')
+        }),
+        ('User permissions',{
+            'fields':('is_staff','is_superuser')
+        })
+    )
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,9 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    pass
+
+    phone_number = models.CharField(max_length=255, blank=True,null=True)
+    
 
     def __str__(self):
-        return self.email
+        return self.username
